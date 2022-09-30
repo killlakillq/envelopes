40865a152bf71f3d56ba97516fd45a8228a732c6
maybe today <100804873+killlakillq@users.noreply.github.com>
2022-10-01 00:44:04 +0300

    Update db.js

--- helpers/db.js
+++ helpers/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-  user: 'postgres',
-  password: 'postgres',
-  host: 'localhost',
-  port: '5432',
-  database: 'envelopes',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 
